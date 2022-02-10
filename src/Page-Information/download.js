68c35c3481db77644f138248f0ce06c534efa7cf
Bryan Wandrych <bdwandry@mtu.edu>
2022-02-10 12:48:05 -0500

    Starting a new Download page for hosting files

--- src/Page-Information/download.js
+++ src/Page-Information/download.js
@@ -10,9 +10,22 @@ const Download = ({handlePageLocation, handlePictureChange, changeFavicon}) => {
         document.title = "Downloads"
     }, [])
 
+    const TermuxFiles = [
+        {Text: "Exagear - Complete with XFCE + Wine 7", location: "https://bryanwandrych.com/Downloads/Termux/Files/Exagear-Debian-Buster-Termux.zip", Description: "Not Yet Setup"},
+    ]
+
     return (
         <div>
-            <h1>Download</h1>
+            <h1>Download <a href={"https://termux.com/"}>Termux-App</a> Based Scripts & Files</h1>
+            <h2>Scripts: </h2>
+
+            <h2>Files: </h2>
+            {TermuxFiles.map((Text) => (
+                <div>
+                    <h3><li>{Text.Text}: <a href={Text.location}>Download</a></li> </h3>
+                </div>
+            ))}
+            <hr/>
         </div>
     );
 };
